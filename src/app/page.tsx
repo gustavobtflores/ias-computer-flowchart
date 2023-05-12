78d8fb349ad71c416accbcfb0ff7553cf18ccc30
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-05-12 18:22:20 -0300

    Basic structure to test viability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,81 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+
+import RhombusNode from "@/components/RhombusNode";
+import ReactFlow, { Background, Controls, MarkerType } from "reactflow";
+import "reactflow/dist/style.css";
+import "../styles.css";
+
+const edges = [
+  {
+    id: "1-2",
+    source: "start",
+    target: "nextinstruction",
+    label: "",
+  },
+  {
+    id: "2-3",
+    source: "nextinstruction",
+    target: "yes",
+    label: "Sim",
+    sourceHandle: "nextinstruction.left",
+    targetHandle: "yes.right",
+  },
+  {
+    id: "2-5",
+    source: "nextinstruction",
+    target: "pcinc",
+    sourceHandle: "nextinstruction.bottom",
+    targetHandle: "yes.right",
+  },
+  {
+    id: "2-4",
+    source: "yes",
+    target: "pcinc",
+    sourceHandle: "nextinstruction.bottom",
+  },
+  {
+    id: "4-5",
+    source: "pcinc",
+    target: "decode",
+  },
+  {
+    id: "5-6",
+    source: "decode",
+    target: "acinc",
+    type: "step",
+    sourceHandle: "decode.bottom",
+  },
+];
+
+const nodes = [
+  {
+    id: "start",
+    position: { x: 0, y: 0 },
+    data: { label: "Início" },
+  },
+  {
+    id: "nextinstruction",
+    position: { x: 12.5, y: 100 },
+    data: { label: "A próxima instrução está no IR?" },
+    type: "rhombusNode",
+  },
+  { id: "yes", position: { x: -300, y: 137.5 }, data: { label: "IR <-- IBR(0:7)\nMAR <-- IBR(8:19)" } },
+  { id: "pcinc", position: { x: 0, y: 400 }, data: { label: "PC <-- PC + 1" } },
+  { id: "decode", position: { x: 0, y: 600 }, data: { label: "Decodificar instrução" } },
+  { id: "acinc", position: { x: -200, y: 800 }, data: { label: "ADD M(X)\n(00000101)" }, type: "rhombusNode" },
+];
+
+const nodeTypes = {
+  rhombusNode: RhombusNode,
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    <div style={{ width: "100%", height: "100vh" }}>
+      <ReactFlow fitView nodes={nodes} edges={edges} nodeTypes={nodeTypes} defaultEdgeOptions={{ type: "step", markerEnd: { type: MarkerType.ArrowClosed } }}>
+        <Background />
+        <Controls />
+      </ReactFlow>
+    </div>
+  );
 }
---
+++ src/components/RhombusNode.tsx
@@ -0,0 +1,18 @@
+import { Handle, Position } from "reactflow";
+
+export default function RhombusNode({ data, id, width, height }: any) {
+  return (
+    <div className="rhombusNode" style={{ maxHeight: height, maxWidth: width }}>
+      <div className="rhombusNode-label">
+        <svg viewBox="0 0 100 100" style={{ display: "block", overflow: "visible" }}>
+          <path d="M0,50 L50,0 L100,50 L50,100 z" fill="#ffffff" stroke="#222222" stroke-width="1" strokeLinecap="round"></path>
+        </svg>
+        <span>{data.label}</span>
+      </div>
+      <Handle type="target" position={Position.Top} id={`${id}.top`} />
+      <Handle type="source" position={Position.Bottom} id={`${id}.bottom`} />
+      <Handle type="source" position={Position.Left} id={`${id}.left`} />
+      <Handle type="source" position={Position.Right} id={`${id}.right`} />
+    </div>
+  );
+}
