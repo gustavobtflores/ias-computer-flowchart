aa21696a0af1bafb137d194f4813411f56135e62
Gustavo Bernardo <gustavobtflores@gmail.com>
2023-05-13 01:25:12 -0300

    Better id's naming and all arithmetic instructions

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ import "../styles.css";
 
 const edges = [
   {
-    id: "1-2",
+    id: "start-nextinstruction",
     source: "start",
     target: "nextinstruction",
     label: "",
   },
   {
-    id: "2-3",
+    id: "nextinstruction-yes",
     source: "nextinstruction",
     target: "yes",
     label: "Sim",
@@ -21,48 +21,80 @@ const edges = [
     targetHandle: "yes.right",
   },
   {
-    id: "2-5",
+    id: "nextinstruction-pcinc",
     source: "nextinstruction",
     target: "pcinc",
     sourceHandle: "nextinstruction.bottom",
-    targetHandle: "yes.right",
+    targetHandle: "pcinc.right",
   },
   {
-    id: "2-4",
+    id: "yes-pcinc",
     source: "yes",
     target: "pcinc",
     sourceHandle: "nextinstruction.bottom",
   },
   {
-    id: "4-5",
+    id: "pcinc-decode",
     source: "pcinc",
     target: "decode",
   },
   {
-    id: "5-6",
+    id: "decode-addmx",
     source: "decode",
-    target: "acinc",
-    type: "step",
+    target: "addmx",
     sourceHandle: "decode.bottom",
+  },
+  {
+    id: "decode-submx",
+    source: "decode",
+    target: "submx",
+  },
+  {
+    id: "decode-mulmx",
+    source: "decode",
+    target: "mulmx",
+  },
+  {
+    id: "decode-divmx",
+    source: "decode",
+    target: "divmx",
+  },
+  {
+    id: "decode-addmodmx",
+    source: "decode",
+    target: "addmodmx",
+  },
+  {
+    id: "decode-submodmx",
+    source: "decode",
+    target: "submodmx",
+  },
+  {
+    id: "decode-lsh",
+    source: "decode",
+    target: "lsh",
+  },
+  {
+    id: "decode-rsh",
+    source: "decode",
+    target: "rsh",
   },
 ];
 
 const nodes = [
-  {
-    id: "start",
-    position: { x: 0, y: 0 },
-    data: { label: "Início" },
-  },
-  {
-    id: "nextinstruction",
-    position: { x: 12.5, y: 100 },
-    data: { label: "A próxima instrução está no IR?" },
-    type: "rhombusNode",
-  },
-  { id: "yes", position: { x: -300, y: 137.5 }, data: { label: "IR <-- IBR(0:7)\nMAR <-- IBR(8:19)" } },
+  { id: "start", position: { x: 0, y: 0 }, data: { label: "Início" } },
+  { id: "nextinstruction", position: { x: 12.5, y: 100 }, data: { label: "A próxima instrução está no IR?" }, type: "rhombusNode" },
+  { id: "yes", position: { x: -300, y: 137.5 }, data: { label: "IR <-- IBR(0:7)\nMAR <-- IBR(8:19)" }, targetPosition: "right" },
   { id: "pcinc", position: { x: 0, y: 400 }, data: { label: "PC <-- PC + 1" } },
   { id: "decode", position: { x: 0, y: 600 }, data: { label: "Decodificar instrução" } },
-  { id: "acinc", position: { x: -200, y: 800 }, data: { label: "ADD M(X)\n(00000101)" }, type: "rhombusNode" },
+  { id: "addmx", position: { x: -600 + 12.5, y: 800 }, data: { label: "ADD M(X)\n(00000101)" }, type: "rhombusNode" },
+  { id: "addmodmx", position: { x: -400 + 12.5, y: 800 }, data: { label: "ADD |M(X)|\n(00000101)" }, type: "rhombusNode" },
+  { id: "submx", position: { x: -200 + 12.5, y: 800 }, data: { label: "SUB M(X)\n(00000110)" }, type: "rhombusNode" },
+  { id: "submodmx", position: { x: -0 + 12.5, y: 800 }, data: { label: "SUB |M(X)|\n(00000110)" }, type: "rhombusNode" },
+  { id: "mulmx", position: { x: 200 + 12.5, y: 800 }, data: { label: "MUL M(X)\n(00000110)" }, type: "rhombusNode" },
+  { id: "divmx", position: { x: 400 + 12.5, y: 800 }, data: { label: "DIV M(X)\n(00000110)" }, type: "rhombusNode" },
+  { id: "lsh", position: { x: 600 + 12.5, y: 800 }, data: { label: "LSH\n(00000110)" }, type: "rhombusNode" },
+  { id: "rsh", position: { x: 800 + 12.5, y: 800 }, data: { label: "RSH\n(00000110)" }, type: "rhombusNode" },
 ];
 
 const nodeTypes = {
